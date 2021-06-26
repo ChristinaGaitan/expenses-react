4f6ce94550efcfa20131770e43f665a0cc73175e
ChristinaGaitan <christina.gaitan@gmail.com>
2021-06-26 16:08:04 -0500

    Add binding to form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,7 +45,12 @@ const ExpenseForm = () => {
       date: new Date(userInput.enteredDate),
     };
 
-    console.log('expense data', expenseData)
+    setUserInput({
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
+    });
+    console.log("expense data", expenseData);
   };
 
   return (
@@ -53,7 +58,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
@@ -62,6 +71,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={userInput.enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -72,6 +82,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={userInput.enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
